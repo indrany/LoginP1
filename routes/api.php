6fcde6459c1a2841b0d03ca3d91f63fa5f25e807
indrany <indranyapriliana@gmail.com>
2024-04-22 11:05:42 +0700

    feat(routes-api) update

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
-Route::post("register", [AuthController::class,"register"]);
-Route::post("login", [AuthController::class,"login"]);
+Route::get("register", [AuthController::class,"register"]);
+Route::get("login", [AuthController::class,"login"]);
 
 Route::group([
     "middleware" => ["auth:sanctum"]
 ], function (){
+    route::post("status",[AuthController::class,"status"]);
     Route::get("profile", [AuthController::class,"profile"]);
     Route::get("logout", [AuthController::class,"logout"]);
 });
-
-
