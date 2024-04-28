61f9a3ff73ae369cf4cd96504613ad30d767d3ec
Adk_alfin <alfinalfarisi7@gmail.com>
2024-04-28 22:22:05 +0700

    refact: user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +10,13 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Status constants
+     */
+    const STATUS_PENDING = 0;
+    const STATUS_ACCEPTED = 1;
+    const STATUS_REJECTED = 2;
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +28,6 @@ class User extends Authenticatable
         'email',
         'password',
         'status'
-        
     ];
 
     /**
